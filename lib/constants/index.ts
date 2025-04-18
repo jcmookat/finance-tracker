b48a3b4130fd1ffe7da3deb2396ec75f6d772132
Jose Cardenas <jscardenas717@rocketmail.com>
2025-04-18 13:55:46 +0900

    create next app, shadcn setup, root layout and constants

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Gastos ni Grasya';
+export const APP_DESCRIPTION =
+	process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Tracker of gastos ni Grasya';
+export const SERVER_URL =
+	process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
