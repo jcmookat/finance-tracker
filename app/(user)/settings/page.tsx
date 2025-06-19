b0556e353bed80ef71b1b442a608d5390b14ff82
Jose Cardenas <jscardenas717@rocketmail.com>
2025-06-19 12:59:12 +0900

    test link

--- app/(user)/settings/page.tsx
+++ app/(user)/settings/page.tsx
@@ -1,10 +1,16 @@
 import ModeToggle from '@/components/mode-toggle';
+import Link from 'next/link';
 
 const SettingsPage = () => {
 	return (
 		<div>
 			<h2>Theme</h2>
 			<ModeToggle />
+			<div>
+				<Link href='https://www.facebook.com/mookatph'>
+					Test link to facebook
+				</Link>
+			</div>
 		</div>
 	);
 };
