8bc495baf7b0d175e5530334fc8a3fa75eb08eb6
Jose Cardenas <jscardenas717@rocketmail.com>
2025-08-16 13:19:24 +0900

    middleware reduce

--- middleware.ts
+++ middleware.ts
@@ -4,36 +4,36 @@ import { privateRoutes } from './routes';
 
 const { auth } = NextAuth(authConfig);
 export default auth(async (req) => {
-  const isLoggedIn = !!req.auth;
-  const { nextUrl } = req;
+	const isLoggedIn = !!req.auth;
+	const { nextUrl } = req;
 
-  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = nextUrl.pathname.includes('/auth');
-  const isApiRoute = nextUrl.pathname.includes('/api');
-  const isHomePage = nextUrl.pathname === '/';
+	const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
+	const isAuthRoute = nextUrl.pathname.includes('/auth');
+	const isApiRoute = nextUrl.pathname.includes('/api');
+	// const isHomePage = nextUrl.pathname === '/';
 
-  if (isApiRoute) {
-    return;
-  }
+	if (isApiRoute) {
+		return;
+	}
 
-  // Redirect from homepage to dashboard when logged in
-  if (isLoggedIn && isHomePage) {
-    return Response.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard`);
-  }
+	// Redirect from homepage to dashboard when logged in
+	// if (isLoggedIn && isHomePage) {
+	//   return Response.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard`);
+	// }
 
-  if (isLoggedIn && isAuthRoute) {
-    return Response.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard`);
-  }
+	// if (isLoggedIn && isAuthRoute) {
+	//   return Response.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard`);
+	// }
 
-  if (isAuthRoute && !isLoggedIn) {
-    return;
-  }
+	if (isAuthRoute && !isLoggedIn) {
+		return;
+	}
 
-  if (!isLoggedIn && isPrivateRoute) {
-    return Response.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/sign-in`);
-  }
+	if (!isLoggedIn && isPrivateRoute) {
+		return Response.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/sign-in`);
+	}
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
